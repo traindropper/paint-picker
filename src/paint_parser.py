46cc89a1554a75a810ade771ea03a9bb680511eb
traindropper <brettreeder@protonmail.com>
2025-07-26 15:44:37 -0700

    initial parsing of name

--- src/paint_parser.py
+++ src/paint_parser.py
@@ -2,14 +2,22 @@ import glob
 from pathlib import Path
 import matplotlib.pyplot as plt
 import cv2
+import pytesseract
+
 
 def main():
+    path_to_tesseract = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+    print(path_to_tesseract)
+    # Providing the tesseract executable
+    # location to pytesseract library
+    pytesseract.tesseract_cmd = path_to_tesseract
+
     image_tests_path: Path = Path(__file__).parent.parent / "tests" / "paints"
     image_paths: list[Path | None] = [x for x in image_tests_path.glob('**/*') if x.is_file()]
     if not image_paths:
         print("No image files found in the specified directory.")
         return
-    Path.mkdir(image_tests_path.parent / "grey_images", exist_ok=True)
+
     for image_path in image_paths:
         print(f"Processing image: {image_path.name}")
         image = cv2.imread(str(image_path))
@@ -17,24 +25,24 @@ def main():
             print(f"Failed to read image: {image_path.name}")
             continue
         
-        # Example processing: Convert to grayscale
+        # Example processing: Convert to grayscale, blur, then threshold
         gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        edges = cv2.Canny(gray_image, 100, 200)
+        blurred_image = cv2.GaussianBlur(gray_image, (3,3), 0)
+        thresholded_image = cv2.threshold(
+            blurred_image, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU
+        )[1]
 
-        
-        # Save the processed image (optional)
-        output_path = image_tests_path.parent / "grey_images" / f"gray_{image_path.name}"
-        cv2.imwrite(str(output_path), gray_image)
-        print(f"Saved processed image to: {output_path}")
+        # Clean up the image with opening (erode then dilate)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1,1))
+        opening = cv2.morphologyEx(thresholded_image, cv2.MORPH_OPEN, kernel, iterations=1)
+        invert = 255 - opening
 
-        plt.figure(figsize=(10, 5))
-        plt.subplot(1, 2, 1)
-        plt.title('Original Image')
-        plt.imshow(image, cmap='gray')
-        plt.subplot(1, 2, 2)
-        plt.title('Edges')
-        plt.imshow(edges, cmap='gray')
-        plt.show()
+        cv2.imshow("grayscale", invert)
+        cv2.waitKey(0)
+        text = pytesseract.image_to_string(invert, lang="eng")
+        # Displaying the extracted text
+        print(text)
+        # edges = cv2.Canny(gray_image, 100, 200)
 
 if __name__ == "__main__":
     main()
