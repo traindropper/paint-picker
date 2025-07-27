a7357dcb8ca3a49aa7be715b0ee3dbced777d920
Train Dropper <brettreeder@protonmail.com>
2025-07-27 16:49:53 -0700

    update venv naming

--- create_venv.sh
+++ create_venv.sh
@@ -4,6 +4,11 @@ EDITABLE="true"
 
 echo "Using '$VENV_NAME' for virtual environment."
 
+uv venv \
+    --relocatable \
+    --link-mode "copy" \
+    $VENV_NAME
+
 UV_EXTRAS=""
 if [ "$EDITABLE" == "false" ]; then
     UV_EXTRAS="--no-editable"
