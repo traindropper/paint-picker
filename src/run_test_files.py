a182a6b0b009e8138d5defac6289db1686ad1314
Train Dropper <brettreeder@protonmail.com>
2025-08-16 09:50:06 -0700

    remove ocr

--- src/run_test_files.py
+++
@@ -1,9 +0,0 @@
-from _setup import setup_logging
-setup_logging()
-
-from src.paint_parser import parse_test_directory
-
-if __name__ == "__main__":
-    import logging
-    logging.getLogger(__name__).info("Test log from main.py")
-    parse_test_directory()
